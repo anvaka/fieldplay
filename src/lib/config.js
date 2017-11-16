526e7dc647ffecb0927b5b7ad19895b94d147b1f
Andriy Kashcha <anvaka@gmail.com>
2017-11-15 22:22:11 -0800

    Small note about lines

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,5 +1,7 @@
 export default {
   // I need to flash our more details before making any promises.
   isAudioEnabled: false,
+  // this allows to render an overlay grid with vectors. Enable it
+  // and drag the scene a little bit.
   vectorLinesEnabled: false
 }
