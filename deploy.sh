d8861b7802a6c8c16ecbf48b0b9410535fe1ad83
Andriy Kashcha <anvaka@gmail.com>
2017-11-16 00:06:25 -0800

    Tag release with current date

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,6 @@ git init
 git add .
 git commit -m 'push to gh-pages'
 git push --force git@github.com:anvaka/fieldplay.git master:gh-pages
+cd ../
+git tag `date "+release-%Y%m%d%H%M%S"`
+git push --tags
