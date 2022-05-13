c09d7eb575bbb22d58169ed338e3d89050815fab
anvaka <anvaka@gmail.com>
2022-05-12 20:08:04 -0700

    updated link

--- src/components/help/Syntax.vue
+++ src/components/help/Syntax.vue
@@ -4,9 +4,8 @@
     <p>Two most important things you need to know:
       <ul>
         <li>GLSL is strictly typed. Each number <b>must have</b> a dot. E.g. "42" should be written as "42."</li>
-        <li>List of available built-in functions is <a href='https://web.archive.org/web/20211124233141/https://www.shaderific.com/glsl-functions/' target='_blank'>available here</a> </li>
+        <li>List of available built-in functions is <a href='https://shaderific.com/glsl.html' target='_blank'>available here</a> </li>
       </ul>
-      Once you played with standard functions, understanding <a href='https://web.archive.org/web/20191127231803/http://www.shaderific.com/glsl-types' target='_blank'>glsl's types</a> can be useful.
     </p>
     <a href="#" @click.prevent='close' class='close'>Close help</a>
   </div>
